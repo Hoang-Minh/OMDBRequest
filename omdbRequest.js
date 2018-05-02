9e44ec03aba39ac102dba88e46fb691cb12a14e2
Hoang-Minh <quataoxanh83@gmail.com>
2018-05-02 00:09:12 -0700

    output omdb rating

---
+++ omdbRequest.js
@@ -0,0 +1,12 @@
+var request = require("request");
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+    //console.log(JSOn.parse(body));
+    console.log(JSON.parse(body));
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+  }
+});
